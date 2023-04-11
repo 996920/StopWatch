6918e60c562f93eb37249650f249b6e29a88b84c
Rohit Dubey <rohit.dubey083@gmail.com>
2023-04-11 16:07:32 +0530

    StopWatch Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [time, setTime] = useState({ hr: 0, min: 0, sec: 0 });
+
+  useEffect(() => {
+    return () => clearInterval(id.current);
+  }, []);
+
+  let id = useRef();
+
+  function handleTime() {
+    id.current = setInterval(() => {
+      setTime((prev) => {
+        if (prev.sec == 60) {
+          return { ...prev, min: prev.min + 1, sec: 0 };
+        }
+        if (prev.min == 60) {
+          return { ...prev, hr: prev.hr + 1, min: 0, sec: 0 };
+        }
+        let newState = { ...prev, sec: prev.sec + 1 };
+        return newState;
+      });
+      // console.log(time);
+    }, 100);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stop Watch</h1>
+      <h1>
+        {time.hr.toLocaleString("en-us", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+        :
+        {time.min.toLocaleString("en-us", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+        :
+        {time.sec.toLocaleString("en-us", {
+          minimumIntegerDigits: 2,
+          useGrouping: false,
+        })}
+      </h1>
+      <button onClick={() => handleTime()}>Start</button>
+      <button onClick={() => clearInterval(id.current)}>Pause</button>
+      <button
+        onClick={() => {
+          clearInterval(id.current);
+          setTime(0);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 }
